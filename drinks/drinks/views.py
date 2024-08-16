78a37c4c64a8eea6280da30dafd932d66e02e380
AndreGomesN <gomes10andre@gmail.com>
2024-08-16 17:43:04 -0300

    feat: get list of data API

---
+++ drinks/drinks/views.py
@@ -0,0 +1,8 @@
+from django.http import JsonResponse
+from .models import Drink
+from .serializers import DrinkSerializer
+
+def drinks_page(request):
+    drinks = Drink.objects.all()
+    serializer = DrinkSerializer(drinks, many=True)
+    return JsonResponse(serializer.data, safe=False)
